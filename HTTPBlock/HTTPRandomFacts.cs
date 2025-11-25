0dea2a4e00f23cd09cfb227862dddff8c4301821
4090rt <artem2007yannurow@gmail.com>
2025-11-25 22:48:48 +0500

    Add files via upload

---
+++ HTTPBlock/HTTPRandomFacts.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace HTTPBlock
+{
+
+    public class Fact
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class HTTPRandomFacts
+    {
+        string URL = "https://tech-news-api.vercel.app/latest";
+        object lockObj = new object();
+
+        public async Task RandomnewsHttp()
+        {
+            MessageBox.Show("sddf");
+            PoolHttp pool = new PoolHttp();
+            HttpClient client = null;
+            var resultsss = new List<object>();
+            try
+            {
+                using (client = await pool.PoolConnect())
+                {
+                        HttpResponseMessage recpon = await client.GetAsync(URL);
+                        recpon.EnsureSuccessStatusCode();
+                        MessageBox.Show("sddf");
+
+                        if (recpon.IsSuccessStatusCode)
+                        {
+                            var resulthttp = await recpon.Content.ReadAsStringAsync();
+                            var resultserialis = JsonSerializer.Deserialize<List<Fact>>(resulthttp);
+                            MessageBox.Show(resulthttp);
+                            Type type = resulthttp.GetType();
+                            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                            Parallel.ForEach(properties, prop =>
+                            {
+                                var value = prop.GetValue(resulthttp);
+                                lock (lockObj)
+                                {
+                                    resultsss.Add(value);
+                                }
+                            });
+                        }
+                }
+                MessageBox.Show($"{resultsss}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка Http запрос в  HTTPBlock -> HTTPRandomFacts -> HTTPRandomFacts -> RandomnewsHttp" + ex.Message);
+            }
+            finally
+            {
+                pool.PoolDisConnect(client);
+            }
+        }
+    }
+}
