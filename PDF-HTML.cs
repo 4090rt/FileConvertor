86a7e57bea8846d19af576a8b90213650df5b641
4090rt <artem2007yannurow@gmail.com>
2025-12-01 21:03:36 +0500

    Add files via upload

---
+++ PDF-HTML.cs
@@ -0,0 +1,128 @@
+﻿using Aspose.Cells;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace cONVERTPDFTEXT
+{
+    public class PDF_HTML
+    {
+        private long _filerazmer;
+        public async Task<string> fILEPATH()
+        {
+            PoolongSaveFileDialog pool = new PoolongSaveFileDialog();
+            SaveFileDialog savefile = null;
+            try
+            {
+                savefile = pool.GetSavefiledialog();
+                savefile.Title = "Выберите файл";
+                savefile.Filter = "PDF Files (*.pdf)|*.pdf|Все файлы (*.*)|*.*";
+                if (savefile.ShowDialog() == true)
+                {
+                    string FilePath = savefile.FileName;
+                    string pathh = System.IO.Path.GetExtension(FilePath);
+
+                    if (pathh.ToLower() == ".pdf")
+                    {
+                        return FilePath;
+                    }
+                    else
+                    {
+                        return "Вы выбрали файл отличный от pdf!";
+                    }
+                }
+                else
+                {
+                    return "Выберите файл для конвертации";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось получить путь к файлу cONVERTPDFTEXT -> PDF-HTML -> filePATH" + ex.Message);
+                return "Ошибка";
+            }
+            finally
+            {
+                pool.RefreshFilegialog(savefile);
+            }
+        }
+
+
+        public async Task converttohtml(string FilePath, string outpath)
+        {
+            try
+            {
+                FileInfo fileinfo = new FileInfo(FilePath);
+                _filerazmer = fileinfo.Length;
+                if (_filerazmer < 524288000)
+                {
+                    Aspose.Pdf.Document pdfDocument = new Aspose.Pdf.Document(FilePath);
+                    pdfDocument.Save(outpath, Aspose.Pdf.SaveFormat.Html);
+                }
+                else
+                {
+                    MessageBox.Show("Файл слишком большой для конвертации");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось конвертировать файл cONVERTPDFTEXT -> PDF-HTML -> converttohtml" + ex.Message);
+            }
+        }
+
+
+        public async Task<string> Savehtml(string FilePath)
+        { 
+            PoolongSaveFileDialog pool = new PoolongSaveFileDialog();
+            SaveFileDialog savedialog = null;
+            try
+            {
+                savedialog = pool.GetSavefiledialog();
+                savedialog.Title = "Сохранение файла";
+                savedialog.Filter = "HTML файлы (*.html)|*.html|Все файлы (*.*)|*.*";
+                if (savedialog.ShowDialog() == true)
+                {
+                    string path = savedialog.FileName;
+                    string pathh = System.IO.Path.GetPathRoot(path);
+                    DriveInfo DRIVE = new DriveInfo(pathh);
+                    long freespace = DRIVE.AvailableFreeSpace;
+                    if (freespace > _filerazmer * 2)
+                    {
+                        string rach = System.IO.Path.GetExtension(path);
+                        if (rach.ToLower() == ".html" || rach.ToLower() == ".htm")
+                        {
+                            await converttohtml(FilePath, path).ConfigureAwait(false);
+                            return path;
+                        }
+                        else
+                        {
+                            return "Попытка сохранить отличный от html файл";
+                        }
+                    }
+                    else
+                    {
+                        return "На диске недостаточно места для сохранения";
+                    }
+                }
+                else
+                {
+                    return ("Выберите файл для сохранения");
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Не удалось сохранить файл cONVERTPDFTEXT -> PDF - HTML -> Savehtml" + ex.Message;
+            }
+            finally
+            {
+                pool.RefreshFilegialog(savedialog);
+            }
+        }
+    }
+}
---
+++ Word - HTML.cs
@@ -0,0 +1,124 @@
+﻿using Aspose.Words;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cONVERTPDFTEXT
+{
+    public class Word___HTML
+    {
+        private long _filerazmer;
+
+        public async Task<string> Filepath()
+        {
+            PoolongSaveFileDialog pool = new PoolongSaveFileDialog();
+            SaveFileDialog saveFileDialog = null;
+            try
+            {
+                saveFileDialog = pool.GetSavefiledialog();
+                saveFileDialog.Title = "Выбор файла";
+                saveFileDialog.Filter = "Word Documents (*.docx)|*.docx|Word 97-2003 (*.doc)|*.doc|Все файлы (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string FilePath = saveFileDialog.FileName;
+                    string pathh = System.IO.Path.GetExtension(FilePath);
+                    if (pathh.ToLower() == ".docx")
+                    {
+                        return FilePath;
+                    }
+                    else
+                    {
+                        return "Неверное расширение файла!";
+                    }
+                }
+                else
+                {
+                    return "Файл не выбран!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось получить путь к файлу cONVERTPDFTEXT -> Word-Text -> Fipepath" + ex.Message);
+                return "Ошибка";
+            }
+            finally
+            {
+                pool.RefreshFilegialog(saveFileDialog);
+            }
+        }
+        public async Task converthtml(string FilePath, string outpath)
+        {
+            try
+            {
+               FileInfo fileonfo = new FileInfo(FilePath);
+                _filerazmer = fileonfo.Length;
+                if (_filerazmer < 524288000)
+                {
+                    Aspose.Words.Document document = new Aspose.Words.Document(FilePath);
+                    document.Save(outpath,(Aspose.Words.SaveFormat.Html));
+                }
+                else
+                {
+                    MessageBox.Show("Файл слишком большой для конвертации");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось конвертировать файл cONVERTPDFTEXT -> Word-HTML -> converthtml" + ex.Message);
+            }
+        }
+
+        public async Task<string> savehtml(string FilePaTH)
+        { 
+            PoolongSaveFileDialog pool = new PoolongSaveFileDialog();
+            SaveFileDialog saveFileDialog = null;
+            try
+            {
+                saveFileDialog = pool.GetSavefiledialog();
+                saveFileDialog.Title = "Сохранение файла";
+                saveFileDialog.Filter = "HTML файлы (*.html)|*.html|Все файлы (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string path = saveFileDialog.FileName;
+                    string pathh = Path.GetPathRoot(path);
+                    DriveInfo driveinfo = new DriveInfo(pathh);
+                    long freespace = driveinfo.AvailableFreeSpace;
+                    if (freespace > _filerazmer * 2)
+                    { 
+                        string filerrach = Path.GetExtension(path);
+                        if (filerrach.ToLower() == ".html" || filerrach.ToLower() == ".htm")
+                        { 
+                            await converthtml(FilePaTH, path).ConfigureAwait(false);
+                            return path;
+                        }
+                        else
+                        {
+                            return "Попытка сохранить отличный от html файл";
+                        }
+                    }
+                    else
+                    {
+                        return "На диске недостаточно места для сохранения";
+                    }
+                }
+                else
+                {
+                    return ("Выберите файл для сохранения");
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Не удалось сохранить файл cONVERTPDFTEXT -> PDF - HTML -> Savehtml" + ex.Message;
+            }
+            finally
+            {
+                pool.RefreshFilegialog(saveFileDialog);
+            }
+        }
+    }
+}
